551b76aa541441b484be2f9240f70d9739600a4e
Herin Lakdawala <herin0506@gmail.com>
2023-10-15 23:52:11 +0530

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>WYN</title>
+        <meta name="description" content="WYN app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>WYN app goes here</div>
+      </main>
+    </div>
+  );
+}

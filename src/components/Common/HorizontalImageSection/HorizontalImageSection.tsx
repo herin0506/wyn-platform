f7d7f61a27d965e05928ec5ed513b58e4edd04ca
mehta.yash919 <mehta.yash919@gmail.com>
2023-12-08 22:40:20 +0530

    about us page - updated common component to cater image better and more fixes

--- src/components/Common/HorizontalImageSection/HorizontalImageSection.tsx
+++ src/components/Common/HorizontalImageSection/HorizontalImageSection.tsx
@@ -1,26 +1,46 @@
 import ImageView from '@wyn/components/Common/ImageViewComponent/ImageView';
 import { StyledHorizontalImageSection } from './styled';
 import PropTypes from 'prop-types';
+import useMediaQuery from '@mui/material/useMediaQuery';
 
 interface HorizontalImageSectionComponentProps {
-  src: string;
+  src?: string;
+  desktopSrc?: string;
+  mobileSrc?: string;
   alt: string;
+  desktopBackgroundImage?: string;
+  mobileBackgroundImage?: string;
 }
 
 const HorizontalImageSectionComponent = ({
   src,
   alt,
+  desktopSrc,
+  mobileSrc,
+  desktopBackgroundImage,
+  mobileBackgroundImage,
 }: HorizontalImageSectionComponentProps) => {
+  const isDesktopView = useMediaQuery('(min-width:960px)');
+  const isMobileTabletView = useMediaQuery('(max-width:959px)');
+
   return (
-    <StyledHorizontalImageSection maxWidth="xl">
-      <ImageView src={src} alt="Your Image" />
+    <StyledHorizontalImageSection
+      maxWidth="xl"
+      sx={{
+        ...(isDesktopView && {
+          backgroundImage: `url('${desktopBackgroundImage}')`,
+        }),
+        ...(isMobileTabletView && {
+          backgroundImage: `url('${mobileBackgroundImage}')`,
+        }),
+      }}
+    >
+      <ImageView
+        src={(isDesktopView ? desktopSrc : mobileSrc) || src || ''}
+        alt={alt}
+      />
     </StyledHorizontalImageSection>
   );
-};
-
-HorizontalImageSectionComponent.propTypes = {
-  src: PropTypes.string.isRequired,
-  alt: PropTypes.string.isRequired,
 };
 
 export default HorizontalImageSectionComponent;

1f89d3dde63aed0661c256a2c7650685eff72369
Pratik2601200 <pratikkolse242@gmail.com>
2023-12-11 23:17:32 +0530

    optimize code

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,43 +1,34 @@
 import React from 'react';
-import { Box, Container,Grid, Hidden, Typography } from '@mui/material';
+import {  Container,Grid,Typography } from '@mui/material';
 import BannerComponent from '@wyn/components/Common/Banner/Banner';
 import { Fragment } from 'react';
 import {Blog1,Blog2,Blog3,Blog4,Blog5} from '@wyn/components/Blog/Blog1'; 
 
 
 
+const blogs = [Blog1, Blog2, Blog3, Blog4, Blog5];
+
 const BlogPage = () => {
-  return ( 
-  <Fragment>
-    <BannerComponent
-      alt="main-banner"
-      desktopImgUrl="/images/blog-banner.png"
-      mobileImgUrl="/images/blog_mobile_banner.png"
-    />
-    <Container maxWidth="xl">
-      <Typography variant='h4' pt={5} pb={2} mb={2} sx={{borderBottom:"1px solid black"}}>
-        Our Blog posts
-      </Typography>
-      <Grid container spacing={2} pb={8}>
-         
-          <Grid item xs={12} pb={2} lg={4} md={6} sm={6} xl={4}>
-            <Blog1 />
-          </Grid>
-          <Grid item xs={12} lg={4} md={6} sm={6} xl={4}>
-            <Blog2 />
-          </Grid>
-          <Grid item xs={12} lg={4} md={6} sm={6} xl={4}>
-            <Blog3 />
-          </Grid>
-          <Grid item xs={12} lg={4} md={6} sm={6} xl={4}>
-            <Blog4 />
-          </Grid>
-          <Grid item xs={12} lg={4} md={6} sm={6} xl={4}>
-            <Blog5 />
-          </Grid>
+  return (
+    <Fragment>
+      <BannerComponent
+        alt="main-banner"
+        desktopImgUrl="/images/blog-banner.png"
+        mobileImgUrl="/images/blog_mobile_banner.png"
+      />
+      <Container maxWidth="xl">
+        <Typography variant='h4' pt={5} pb={2} mb={2} sx={{ borderBottom: "1px solid black" }}>
+          Our Blog posts
+        </Typography>
+        <Grid container spacing={2} pb={8}>
+          {blogs.map((Blog, index) => (
+            <Grid key={index} item xs={12} lg={4} md={6} sm={6} xl={4}>
+              <Blog />
+            </Grid>
+          ))}
         </Grid>
-    </Container>
-  </Fragment>
+      </Container>
+    </Fragment>
   );
 };
 

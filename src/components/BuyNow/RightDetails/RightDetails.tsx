1a13713a050dd872b5ad92e68068dda6ce1202bc
mehta.yash919 <mehta.yash919@gmail.com>
2023-12-17 21:52:19 +0530

    buy now - right section inital skeletion

---
+++ src/components/BuyNow/RightDetails/RightDetails.tsx
@@ -0,0 +1,69 @@
+import { Button, Rating, Typography } from '@mui/material';
+import Divider from '@mui/material/Divider';
+import Grid from '@mui/material/Grid';
+import { APP_COLORS } from '@wyn/styles/colors/colors';
+import { StyledRightDetails } from './styled';
+
+interface RightDetailsComponentProps {
+  category: string;
+  rating: number;
+  title: string;
+  subTitle: string;
+  price: number;
+}
+
+const RightDetailsComponent = ({
+  category,
+  rating,
+  title,
+  subTitle,
+  price,
+}: RightDetailsComponentProps) => {
+  return (
+    <StyledRightDetails maxWidth="xl">
+      <Grid container justifyContent="space-between">
+        <Grid pt={2} item lg={6}>
+          <Typography variant="h6" sx={{ color: APP_COLORS.PINK }}>
+            {category}
+          </Typography>
+        </Grid>
+        <Grid item lg={6} sx={{ display: 'flex', alignItems: 'end' }}>
+          <Typography
+            variant="h6"
+            sx={{ display: 'flex', alignItems: 'center' }}
+          >
+            {rating}/5
+            <Rating size="medium" readOnly name="product-rating" value={4} />
+          </Typography>
+        </Grid>
+      </Grid>
+      <Typography pt={3} variant="h3" mb={2}>
+        {title}
+      </Typography>
+      <Divider />
+      <Typography pt={3} variant="h6" mb={2}>
+        {subTitle}
+      </Typography>
+
+      <Grid container spacing={2}>
+        <Grid item md={12}>
+          <Typography variant="h5" mb={4}>
+            <b>Rs.{price}</b>
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" size="small">
+            Buy now
+          </Button>
+        </Grid>
+        <Grid item>
+          <Button variant="contained" size="small">
+            Add to cart
+          </Button>
+        </Grid>
+      </Grid>
+    </StyledRightDetails>
+  );
+};
+
+export default RightDetailsComponent;
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -430,3 +430,54 @@ export const teamDetails = [
     subTitle: "Sagar Tungare, our Chief Operating Officer, possesses a deep understanding that the foundation of a thriving company lies in smooth operations, much like the harmony of a well-conducted orchestra. His role extends beyond operational management; he actively contributes to shaping our work culture, promoting values like creativity, accountability, and a relentless drive for improvement. READ MORE"
   } 
 ]
+
+export const countries = [
+  {
+    value: 'ind',
+    label: 'India'
+  },
+  {
+    value: 'usa',
+    label: 'USA'
+  }
+]
+
+export const states = [
+  {"value": "AN", "label": "Andaman and Nicobar Islands"},
+  {"value": "AP", "label": "Andhra Pradesh"},
+  {"value": "AR", "label": "Arunachal Pradesh"},
+  {"value": "AS", "label": "Assam"},
+  {"value": "BR", "label": "Bihar"},
+  {"value": "CH", "label": "Chandigarh"},
+  {"value": "CT", "label": "Chhattisgarh"},
+  {"value": "DN", "label": "Dadra and Nagar Haveli and Daman and Diu"},
+  {"value": "DL", "label": "Delhi"},
+  {"value": "GA", "label": "Goa"},
+  {"value": "GJ", "label": "Gujarat"},
+  {"value": "HR", "label": "Haryana"},
+  {"value": "HP", "label": "Himachal Pradesh"},
+  {"value": "JK", "label": "Jammu and Kashmir"},
+  {"value": "JH", "label": "Jharkhand"},
+  {"value": "KA", "label": "Karnataka"},
+  {"value": "KL", "label": "Kerala"},
+  {"value": "LD", "label": "Lakshadweep"},
+  {"value": "MP", "label": "Madhya Pradesh"},
+  {"value": "MH", "label": "Maharashtra"},
+  {"value": "MN", "label": "Manipur"},
+  {"value": "ML", "label": "Meghalaya"},
+  {"value": "MZ", "label": "Mizoram"},
+  {"value": "NL", "label": "Nagaland"},
+  {"value": "OD", "label": "Odisha"},
+  {"value": "PY", "label": "Puducherry"},
+  {"value": "PB", "label": "Punjab"},
+  {"value": "RJ", "label": "Rajasthan"},
+  {"value": "SK", "label": "Sikkim"},
+  {"value": "TN", "label": "Tamil Nadu"},
+  {"value": "TS", "label": "Telangana"},
+  {"value": "TR", "label": "Tripura"},
+  {"value": "UP", "label": "Uttar Pradesh"},
+  {"value": "UK", "label": "Uttarakhand"},
+  {"value": "WB", "label": "West Bengal"}
+]
+
+export const termsAndCondition = "By signing up via text, you agree to receive recurring automated marketing messages, including cart reminders, at the phone number provided. Consent is not a condition of purchase. Reply STOP to unsubscribe. Reply HELP for help. Message frequency varies. Msg & data rates may apply. View our Privacy Policy and Terms of Service.";
